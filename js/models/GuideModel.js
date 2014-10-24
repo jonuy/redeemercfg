93cd43f67671c3f45a4be8600596083525ea720d
jon uy <jonxuy@gmail.com>
2014-10-24 19:45:57 -0400

    Content update for 10-24-2014

--- js/models/GuideModel.js
+++ js/models/GuideModel.js
@@ -7,7 +7,7 @@ define(
 
   function($, _, Backbone) {
     var GuideModel = Backbone.Model.extend({
-      url: 'data/2014/10_10_2014.json',
+      url: 'data/2014/10_24_2014.json',
       initialize: function() {
       },
     });
